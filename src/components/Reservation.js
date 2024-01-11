f8d03e8c394d9c625fca72188653e975920b307c
Sarah Otmane <sarahotmane@Air-de-Sarah.lan>
2024-01-11 13:04:29 +0100

    modification de la section info

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -9,25 +9,25 @@ function Reservation(){
         <>
             <main className="reservation h-100 back_vert">
                 <section className='section1 w-100'>
-                        <center>
-                            <img src={reservation} alt='' />
-                        </center>
-                        <article>
-                            <span>Vous voulez gouter nos plats ?</span>
-                            <h1>Réservez maintenant !</h1>
-                        </article>
-                    </section>
+                    <center>
+                        <img src={reservation} alt='' />
+                    </center>
+                    <article>
+                        <span>Vous voulez gouter nos plats ?</span>
+                        <h1>Réservez maintenant !</h1>
+                    </article>
+                </section>
 
-                    <section className="info">
-                        <center><img src={reserv_info} alt='' /></center>
-                        <hr></hr>
-                        <h2 className="w-max-c m-auto ">Réservez une table !</h2>
-                        <article>
-                            <p className="w-max-c m-auto">Nous sommes désolé ! La réservation en ligne est pour le moment indisponible.</p>
-                            <p className="w-max-c m-auto">Appelez nous au <a href='tel:0325466712' className="link">03 25 46 67 12 </a>pour faire votre réservation.</p>
-                        </article>
-                    </section>
-                </main>
+                <section className="info">
+                    <center><img src={reserv_info} alt='' /></center>
+                    <hr></hr>
+                    <h2 className="w-max-c m-auto ">Réservez une table !</h2>
+                    <article>
+                        {/* <p className="w-max-c m-auto">Nous sommes désolé ! La réservation en ligne est pour le moment indisponible.</p> */}
+                        <p className="w-max-c m-auto">Appelez nous au <a href='tel:0325466712' className="link">03 25 46 67 12 </a>pour faire votre réservation.</p>
+                    </article>
+                </section>
+            </main>
             <Footer />
         </>
     )
